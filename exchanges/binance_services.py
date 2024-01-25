85809de1260242f28b4159ed1717ea671a7c9007
Fedkov Roman <romikfedkov@gmail.com>
2024-01-25 18:26:24 +0300

    v0.0.0 - init and start binance services

---
+++ exchanges/binance_services.py
@@ -0,0 +1,22 @@
+import os
+import asyncio
+import websockets
+from dotenv import load_dotenv
+from binance import AsyncClient
+
+load_dotenv()
+
+
+async def get_account_coins():
+    client = AsyncClient(api_key=os.environ.get('BINANCE_API_PUBLIC'),
+                         api_secret=os.environ.get('BINANCE_API_SECRET'))
+    coins = await client.get_account()
+    coins = [coin for coin in coins['balances'] if float(coin['free'])]
+    await client.close_connection()
+
+async def get_price():
+    pass
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(get_account_coins())
